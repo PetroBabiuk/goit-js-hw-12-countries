8995b69d041387e36779dc31f456d480efa7e039
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-06-23 21:25:58 +0300

    добавляет fetch и шаблон

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+import countriesTpl from './hbs/countries-list.hbs'
+
+const refs = {
+    input: document.querySelector('.js-input'),
+
+}
+
+   const result = fetch(`https://restcountries.eu/rest/v2/name/uk`)
+   .then(response => {
+   return response.json();
+   }).then(countries => {
+       console.log(countries);
+       if (countries.length > 1) {
+           const markUp = countriesTpl(countries);
+           console.log(markUp);
+       }
+       const markUp = countryTpl(countries);
+       
+   }).catch(error => {
+       console.log(error);
+   });
