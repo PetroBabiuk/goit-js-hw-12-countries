58b8454fe567aeca3ea90744d7ecf4f21cb93b7a
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-06-29 13:12:07 +0300

    добавляет стили

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import './sass/main.scss';
 import countriesTpl from './hbs/countries-list.hbs';
 import countryTpl from './hbs/country-card.hbs';
+import fetchCountries from './js/fetchCountries.js';
 var debounce = require('lodash.debounce');
 
-console.log(debounce);
 const refs = {
     input: document.querySelector('.js-input'),
-    container: document.querySelector('.container')
+    countries: document.querySelector('.countries')
 }
 
 refs.input.addEventListener('input', debounce(onInputEnter, 500));
 
 function onInputEnter(e) {
-      const searchQuery = e.target.value;
-            console.log(`значение ${searchQuery}`);
-      return searchQuery;
-}
-
-   const result = fetch(`https://restcountries.eu/rest/v2/name/uk`)
-   .then(response => {
-   return response.json();
-   }).then(countries => {
-       console.log(countries);
+  const searchQuery = e.target.value;
+  fetchCountries(searchQuery)
+      .then(response => {
+      return response.json();
+      }).then(countries => {
        if (countries.length > 1) {
            const markUp = countriesTpl(countries);
-           console.log(markUp);
-           refs.container.insertAdjacentHTML('beforeend', markUp);
+           refs.countries.innerHTML = markUp;
        } else {
            const markUp = countryTpl(countries);
-       console.log(markUp);
-       refs.container.insertAdjacentHTML('beforeend', markUp);
-       }
-       
+       refs.countries.innerHTML = markUp;
+       }      
    }).catch(error => {
        console.log(error);
-   });
+   });;       
+}
+
+
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,3 @@
 export default function fetchCountries(searchQuery) {
-   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-   .then(response => {response.json})
-   .then(countries)
-   .catch(error => {console.log(error)});
+      return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`);
 }
